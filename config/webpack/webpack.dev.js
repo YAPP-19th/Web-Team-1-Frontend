a1540e201147186da79494811c4c47566fcf734d
raejoonee <crj0901@gmail.com>
2021-09-27 23:39:52 +0900

    [GILJOB-18] env: Webpack 기본 환경을 설정한다.
    dev build에서 style-loader를 사용하고, production build에서 MiniCssExtractPlugin를 사용한다.

---
+++ config/webpack/webpack.dev.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const baseConfig = require('./webpack.base');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+const PROJECT_ROOT = path.resolve(__dirname, '../..');
+const TEMPLATE_PATH = path.resolve(PROJECT_ROOT, 'public');
+
+module.exports = merge(baseConfig, {
+  mode: 'development',
+  devtool: 'eval-source-map',
+  devServer: {
+    hot: true,
+    port: '5050',
+    host: '127.0.0.1',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sassOptions: {
+                includePaths: [path.join(PROJECT_ROOT, './src/styles')],
+              },
+              additionalData: `@import "index";`,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.resolve(TEMPLATE_PATH, 'index.html'),
+      minify: {
+        removeComments: true,
+      },
+    }),
+  ],
+});
---
+++ config/webpack/webpack.prod.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const baseConfig = require('./webpack.base');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const PROJECT_ROOT = path.resolve(__dirname, '../..');
+const TEMPLATE_PATH = path.resolve(PROJECT_ROOT, 'public');
+
+module.exports = merge(baseConfig, {
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sassOptions: {
+                includePaths: [path.join(PROJECT_ROOT, './src/styles')],
+              },
+              additionalData: `@import "index";`,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.resolve(TEMPLATE_PATH, 'index.html'),
+      minify: true,
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+});
