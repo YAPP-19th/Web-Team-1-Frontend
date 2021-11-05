7d9a2d6f1fd2d19381ea53c5fcb5e7c8f9f696e2
raejoonee <crj0901@gmail.com>
2021-11-06 02:28:39 +0900

    [GILJOB-47] feat: 정의한 SCSS를 타입으로 제공한다.
    * 앞서 정의한 SCSS 변수를 타입으로 제공하여 쉽게 사용할 수 있도록 했습니다.

---
+++ src/utils/colorEnum.ts
@@ -0,0 +1,52 @@
+enum ColorMap {
+  // Main color
+  $gilBlue = '#0389ff',
+  $jobNavy = '#172965',
+
+  // System Color
+  $activeGreen = '#69db7c',
+  $inactiveRed = '#ff8787',
+  $warningRed1 = '#fff0ee',
+  $warningRed2 = '#f8d0d1',
+
+  // Gray
+  $white = '#ffffff',
+  $gray0 = '#f2f2f2',
+  $gray1 = '#efeff2',
+  $gray2 = '#ebebee',
+  $gray3 = '#e0e0e5',
+  $gray4 = '#cacacc',
+  $gray5 = '#b1b1b3',
+  $gray6 = '#767679',
+  $gray7 = '#444348',
+  $gray8 = '#15141a',
+  $black = '#000000',
+
+  // Red
+  $red5 = '#ff6b6b',
+  $red6 = '#fa5252',
+  $red7 = '#f03e3e',
+  $red8 = '#e03131',
+  $red9 = '#c92a2a',
+  $red10 = '#a51111',
+
+  // Orange
+  $orangeSoft = '#ffece2',
+  $orangeHard = '#f38119',
+
+  // Green
+  $green = '#34c759',
+
+  // Blue
+  $blue0 = '#f3f9ff',
+  $blue1 = '#e6f2ff',
+  $blue2 = '#c5e1ff',
+  $blue3 = '#92c7ff',
+  $blue4 = '#5facff',
+  $blue5 = '#1184ff',
+  $blue6 = '#0c6cd3',
+  $blue7 = '#004c9e',
+  $blue8 = '#003267',
+}
+
+export default ColorMap;
---
+++ src/utils/index.ts
@@ -0,0 +1,6 @@
+// color
+export { default as ColorMap } from './colorMap';
+export type { default as ColorEnum } from './colorEnum';
+
+// font size
+export type { default as FontSize } from './fontSize';
