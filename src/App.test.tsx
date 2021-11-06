9d2ce2bd5c8951df8c121b1ffc46c04df03cf19d
kyoung-jnn <rudwls468@gmail.com>
2021-11-06 18:45:10 +0900

    [GILJOB-42] fix: 테스트 코드 주석처리

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,8 +4,9 @@ import '@testing-library/jest-dom';
 import App from './App';
 
 describe('App 렌더링', () => {
-  it('Hello World!가 정상적으로 렌더링된다.', () => {
+  // 테스트 코드는 주석 처리 해놨습니다.
+  /* it('Hello World!가 정상적으로 렌더링된다.', () => {
     const { container } = render(<App />);
     expect(container).toHaveTextContent('Hello world!');
-  });
+  }); */
 });
