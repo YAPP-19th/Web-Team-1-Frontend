1171babf4abfce8f072de83a49d9636f200e290d
kyoung-jnn <rudwls468@gmail.com>
2021-11-06 18:53:48 +0900

    [GILJOB-42] fix: App.tsx 테스트 코드 수정

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,9 +4,8 @@ import '@testing-library/jest-dom';
 import App from './App';
 
 describe('App 렌더링', () => {
-  // 테스트 코드는 주석 처리 해놨습니다.
-  /* it('Hello World!가 정상적으로 렌더링된다.', () => {
+  it('랜딩 페이지가 정상적으로 렌더링된다.', () => {
     const { container } = render(<App />);
-    expect(container).toHaveTextContent('Hello world!');
-  }); */
+    expect(container).toHaveTextContent('Landing Page (Main)');
+  });
 });
--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Landing: React.FC = () => {
-  return <div>Lading Page (Main)</div>;
+  return <div>Landing Page (Main)</div>;
 };
 
 export default Landing;
