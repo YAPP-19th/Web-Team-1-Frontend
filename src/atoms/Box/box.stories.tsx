782647a72e17bc2cc59689b02fa4c1d44f8e1138
이주해 <leejuhae821@gmail.com>
2021-11-13 00:51:20 +0900

    [GILJOB-54] feat: Box 컴포넌트의 스토리를 만든다.

---
+++ src/atoms/Box/box.stories.tsx
@@ -0,0 +1,30 @@
+import { Story, Meta } from '@storybook/react';
+import Box from '@src/atoms/Box';
+import { BoxProps } from './Box';
+import AchievementBadge from '@src/components/AchievementBadge';
+
+export default {
+  title: 'Atoms/Box',
+  component: Box,
+  parameters: {
+    docs: {
+      description: {
+        component: 'Box',
+      },
+    },
+  },
+  argTypes: {},
+} as Meta;
+
+const Template: Story<BoxProps> = (args) => <Box {...args} />;
+
+export const defaultBox = Template.bind({});
+
+defaultBox.args = {
+  children: (
+    <AchievementBadge
+      title="5급 항해사"
+      description="길잡 신규가입 시 달성"
+    ></AchievementBadge>
+  ),
+};
