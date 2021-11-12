f0c8143b5072dcc15c883b1e61766d2c45f48155
이주해 <leejuhae821@gmail.com>
2021-11-13 00:45:46 +0900

    [GILJOB-54] feat: Achievement 컴포넌트에 img 태그를 추가한다.

--- src/components/AchievementBadge/AchievementBadge.tsx
+++ src/components/AchievementBadge/AchievementBadge.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './style.scss';
 import Text from '@src/atoms/Text';
+import achievement from '@src/assets/images/achievement.svg';
 
 export interface AchievementBadgeProps {
   title: string;
@@ -14,6 +15,13 @@ const AchievementBadge: React.FC<AchievementBadgeProps> = ({
   return (
     <div className="_ACHIEVEMENT_BADGE_">
       {/* <Icon></Icon> */}
+      <img
+        className="Lv1Image"
+        src={achievement}
+        alt="achievementImage"
+        width="200"
+        height="200"
+      />
       <Text
         align="center"
         fontColor="gil-blue"
