4a1ecdab592f709e1841361210d4dd58a55a714c
raejoonee <crj0901@gmail.com>
2021-11-25 01:06:31 +0900

    [GILJOB-69] feat: Button 컴포넌트를 개발한다.

---
+++ src/components/atoms/Button/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import cn from 'classnames';
+import './style.scss';
+
+export interface ButtonProps {
+  innerText: string;
+  buttonColor: 'gil-blue' | 'job-navy' | 'white' | 'kakao';
+  textColor: 'main' | 'white' | 'gil-blue' | 'job-navy';
+  textSize?: 'small' | 'medium' | 'large';
+  hasBorder?: boolean;
+  hasShadow?: boolean;
+  hasFixedWidth?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  innerText,
+  buttonColor,
+  textColor,
+  textSize = 'medium',
+  hasBorder = false,
+  hasShadow = false,
+  hasFixedWidth = false,
+}) => {
+  return (
+    <button
+      className={cn(
+        `_BUTTON_`,
+        `button-color-${buttonColor}`,
+        `text-color-${textColor}`,
+        `text-size-${textSize}`,
+        { hasBorder },
+        hasFixedWidth ? `fixed-${textSize}` : '',
+        { hasShadow },
+      )}
+      type="button"
+    >
+      {innerText}
+    </button>
+  );
+};
+
+export default Button;
--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,3 +1,4 @@
 export { default as Badge } from './Badge';
+export { default as Button } from './Button';
 export { default as SearchBar } from './SearchBar';
 export { default as Text } from './Text';
