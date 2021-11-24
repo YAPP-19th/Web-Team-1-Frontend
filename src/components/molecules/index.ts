b2f188f7dda04e5f72d22d6efc434b8dcbc1d1ec
raejoonee <crj0901@gmail.com>
2021-11-25 01:11:46 +0900

    [GILJOB-71] feat: Paper 컴포넌트를 개발한다.

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -3,3 +3,4 @@ export { default as Card } from './Card';
 export { default as Content } from './Content';
 export { default as Header } from './Header';
 export { default as Footer } from './Footer';
+export { default as Paper } from './Paper';
