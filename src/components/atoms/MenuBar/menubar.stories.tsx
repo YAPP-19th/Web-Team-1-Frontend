e990e967b692eadf37e08ab02288d6d4c9e96627
raejoonee <crj0901@gmail.com>
2021-11-26 19:02:27 +0900

    [GILJOB-73] feat: MenuBar 컴포넌트의 스토리를 작성한다.

---
+++ src/components/atoms/MenuBar/menubar.stories.tsx
@@ -0,0 +1,40 @@
+import { Story, Meta } from '@storybook/react';
+import MenuBar, { MenuBarProps } from '@src/components/atoms/MenuBar';
+
+export default {
+  title: 'Design System/Atoms/MenuBar',
+  component: MenuBar,
+  parameters: {
+    docs: {
+      description: {
+        component: 'MenuBar',
+      },
+    },
+  },
+  argTypes: {
+    menuList: {
+      type: 'MenuItem',
+    },
+  },
+} as Meta;
+
+const Template: Story<MenuBarProps> = (args) => <MenuBar {...args} />;
+
+export const defaultMenuBar = Template.bind({});
+
+defaultMenuBar.args = {
+  menuList: [
+    {
+      name: '퀘스트 전체',
+      count: 49,
+    },
+    {
+      name: 'Front-End',
+      count: 49,
+    },
+    {
+      name: 'Back-End',
+      count: 49,
+    },
+  ],
+};
