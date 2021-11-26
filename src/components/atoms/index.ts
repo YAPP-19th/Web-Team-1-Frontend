f035565e0ef9103e0419a9458e202f2ccfbb11a0
raejoonee <crj0901@gmail.com>
2021-11-26 19:02:58 +0900

    [GILJOB-73] feat: Pagination 컴포넌트를 개발한다.

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -2,5 +2,6 @@ export { default as Badge } from './Badge';
 export { default as Button } from './Button';
 export { default as Icon } from './Icon';
 export { default as MenuBar } from './MenuBar';
+export { default as Pagination } from './Pagination';
 export { default as SearchBar } from './SearchBar';
 export { default as Text } from './Text';
