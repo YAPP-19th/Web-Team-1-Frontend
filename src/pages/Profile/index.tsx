c28a867a9db6795e268aacf6335d62e52c9b13a5
이주해 <leejuhae821@gmail.com>
2021-11-27 19:44:55 +0900

    [GILJOB-54] width, height 값 변경

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import Mypage from './Mypage/Mypage';
+import './style.scss';
 
 const Profile: React.FC = () => {
   return (
-    <div style={{ width: '1020', height: '800' }}>
+    <section className="profile">
       <Mypage
         nickname="호랑이형님"
         level={3}
         job="Frontend"
         selfDescription="aaa"
       />
-    </div>
+    </section>
   );
 };
 
