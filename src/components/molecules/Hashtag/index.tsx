4b55121a8081e2d7d2de2410885bf99b06bb8cc4
kyoung-jnn <rudwls468@gmail.com>
2021-12-04 22:42:43 +0900

    [GILJOB-74] feat: Hashtag 컴포넌트 추가

---
+++ src/components/molecules/Hashtag/index.tsx
@@ -0,0 +1,42 @@
+import React, { useState, useCallback } from 'react';
+import { Input, Button } from '@src/components/atoms';
+import './style.scss';
+
+const Hashtag: React.FC = () => {
+  const [hashtags, setHashtags] = useState<string[]>([
+    '임시',
+    '해시태그',
+    '입니다',
+  ]);
+
+  const onSubmit = useCallback(
+    (value) => {
+      setHashtags([...hashtags, value]);
+    },
+    [hashtags],
+  );
+
+  return (
+    <div className="_HASHTAG_">
+      <Input
+        hasCount
+        inputHeight="wide"
+        placeholder="해시 태그를 입력하세요"
+        onSubmit={onSubmit}
+      />
+      <article className="hashtag_container">
+        {hashtags.map((hashtag) => (
+          <Button
+            key={hashtag}
+            innerText={`#${hashtag}`}
+            buttonColor="main-gray"
+            textColor="dark-gray"
+            textSize="small"
+          />
+        ))}
+      </article>
+    </div>
+  );
+};
+
+export default Hashtag;
