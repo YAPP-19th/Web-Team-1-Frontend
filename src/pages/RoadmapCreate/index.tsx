2e0ca8f303db0be9725846e54d01e1046281a001
kyoung-jnn <rudwls468@gmail.com>
2021-12-11 19:30:46 +0900

    [GILJOB-92] feat: 로드맵 생성 페이지 제작

---
+++ src/pages/RoadmapCreate/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Text, List, Button } from '@src/components/atoms';
+import { roadmapMain } from '@src/pages/RoadmapCreate/roadmap_data.json';
+import map from '@src/assets/images/map.png';
+import RoadmapMain from './RoadmapMain';
+import RoadmapDetail from './RoadmapDetail';
+import './style.scss';
+
+const RoadmapCreate: React.FC = () => {
+  return (
+    <div className="roadmap-background">
+      <section className="roadmap-wrapper">
+        <div className="roadmap-header">
+          <div className="title">
+            <Text className="title-text" fontWeight="bold">
+              로드맵 등록
+            </Text>
+            <List listData={roadmapMain.list} />
+          </div>
+          <img src={map} alt="map" />
+        </div>
+        <RoadmapMain />
+        <RoadmapDetail />
+        <div className="roadmap-footer">
+          <Button
+            innerText="등록"
+            buttonColor="white"
+            textColor="gil-blue"
+            textSize="medium"
+            hasShadow
+          />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default RoadmapCreate;
