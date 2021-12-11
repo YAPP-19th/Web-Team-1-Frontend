3a576ed9b6681db7e78b44b73a6cdb64cb29506f
이주해 <leejuhae821@gmail.com>
2021-12-11 17:08:31 +0900

    [GILJOB-54] feat: TabBar 컴포넌트 생성
    Profile에서 사용하는 TabBar를 컴포넌트로 작성했습니다.

---
+++ src/components/atoms/TabBar/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import cn from 'classnames';
+import './style.scss';
+import { Text } from '@src/components/atoms';
+
+interface TabItem {
+  name: string;
+}
+
+export interface TabBarProps {
+  tabList: TabItem[];
+  align?: 'start' | 'center' | 'end';
+  divider?: boolean;
+}
+
+const TabBar: React.FC<TabBarProps> = ({
+  tabList,
+  align = 'center',
+  divider = true,
+}) => {
+  return (
+    <div className={cn(`_TABBAR_`, `align-${align}`)}>
+      {tabList.map((item, index) => (
+        <>
+          <Text
+            fontColor="main"
+            fontSize="x-large"
+            fontWeight="regular"
+            align="center"
+            key={index}
+          >
+            {item.name}
+          </Text>
+          {divider && index < tabList.length - 1 && (
+            <div className="divider"></div>
+          )}
+        </>
+      ))}
+    </div>
+  );
+};
+
+export default TabBar;
