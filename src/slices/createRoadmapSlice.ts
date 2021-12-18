0040114688df83b7211eb1dfeaf6c117fb6ca2ca
kyoung-jnn <rudwls468@gmail.com>
2021-12-18 11:34:45 +0900

    [GILJOB-111] feat: Redux Toolkit 임시 연결

---
+++ src/slices/createRoadmapSlice.ts
@@ -0,0 +1,33 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { DropdownListType } from '@src/components/atoms/Dropdown';
+
+export interface createRoadmapState {
+  title: string;
+  position: string;
+}
+
+const initialState: createRoadmapState = {
+  title: '',
+  position: '',
+};
+
+export const createRoadmapSlice = createSlice({
+  name: 'createRoadmap',
+  initialState,
+  reducers: {
+    setTitle: (state, action: PayloadAction<string>) => {
+      state.title = action.payload;
+    },
+    setDropdown: (state, action: PayloadAction<DropdownListType>) => {
+      const { type, value } = action.payload;
+      return {
+        ...state,
+        [type as string]: value,
+      };
+    },
+  },
+});
+
+export const createRoadmapSelector = (state: any) => state.quest;
+export const { setTitle, setDropdown } = createRoadmapSlice.actions;
+export default createRoadmapSlice.reducer;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
+import createQusetReducer from '@src/slices/createQuestSlice';
+import createRoadmapReducer from '@src/slices/createRoadmapSlice';
 import { giljobApi } from '@src/services/giljob';
 
 export const store = configureStore({
   reducer: {
+    createQuest: createQusetReducer,
+    createRoadmap: createRoadmapReducer,
     [giljobApi.reducerPath]: giljobApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(giljobApi.middleware),
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }).concat(giljobApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
