e08c54c30e0239713d7485905a80b43919f04c2e
kyoung-jnn <rudwls468@gmail.com>
2021-12-25 03:35:25 +0900

    [GILJOB-130] feat: Redux-persist 적용

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import store from '@src/store';
 import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 import './index.scss';
 import './normalize.scss';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistStore(store)}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root'),
 );
