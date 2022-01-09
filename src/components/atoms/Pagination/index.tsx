a95c52045049a9262dcec6638451b9c66f8925f9
raejoonee <crj0901@gmail.com>
2022-01-10 00:44:54 +0900

    [GILJOB-126] feat: Pagination 컴포넌트에 기능을 추가한다.
    * 클릭했을 때 현재 보고 있는 페이지를 변경하는 기능을 추가했습니다.

--- src/components/atoms/Pagination/index.tsx
+++ src/components/atoms/Pagination/index.tsx
@@ -1,20 +1,29 @@
-import React, { useMemo } from 'react';
+import React, { useCallback, useMemo } from 'react';
+import { useDispatch } from 'react-redux';
 import cn from 'classnames';
 import './style.scss';
 import { Text } from '@src/components/atoms';
+import { setPage } from '@src/slices/questListSlice';
 
 export interface PaginationProps {
   pageSize: number; // 한 페이지에 표시될 데이터의 개수
-  currentId: number; // 현재 참조 중인 데이터의 index
   totalLength: number; // 모든 데이터의 개수
+  currentPage: number; // 현재 클릭된 page
 }
 
 const Pagination: React.FC<PaginationProps> = ({
   pageSize,
-  currentId,
   totalLength,
+  currentPage,
 }) => {
-  // TODO: page를 클릭했을 때, 서버로부터 데이터 요청해서 리렌더링
+  const dispatch = useDispatch();
+  const handleClick = useCallback(
+    ({ target }) => {
+      dispatch(setPage(parseInt(target.innerText, 10) - 1));
+    },
+    [dispatch],
+  );
+
   const totalPage = useMemo<number>(
     () => Math.ceil(totalLength / pageSize),
     [totalLength, pageSize],
@@ -22,14 +31,17 @@ const Pagination: React.FC<PaginationProps> = ({
   return (
     <div className={cn(`_PAGINATION_`)}>
       {[...Array(totalPage)].map((_, index) => (
-        // TODO: 클릭한 page에 대하여 별도의 스타일 적용
         <Text
-          className={cn(`pagination-number`)}
+          className={cn(
+            `pagination-number`,
+            index === currentPage ? 'clicked' : '',
+          )}
           fontColor="gray"
           fontSize="medium"
           fontWeight="regular"
           // eslint-disable-next-line react/no-array-index-key
           key={index}
+          handleClick={handleClick}
         >
           {index + 1}
         </Text>
