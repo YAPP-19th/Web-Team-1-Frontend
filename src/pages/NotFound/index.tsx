cc69a67a951c958e3aa94226d6de919496285491
kyoung-jnn <rudwls468@gmail.com>
2022-01-10 00:09:23 +0900

    [GILJOB-148] feat: NotFound 페이지 개발

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,7 +1,43 @@
 import React from 'react';
+import type { FC } from 'react';
+import { useHistory } from 'react-router-dom';
+import NotFoundImage from '@src/assets/images/notfound.svg';
+import { Text, Button } from '@src/components/atoms';
+import './style.scss';
 
-const NotFound = () => {
-  return <div>NotFound</div>;
+const NotFound: FC = () => {
+  const history = useHistory();
+
+  const handleButtonClick = () => {
+    history.goBack();
+  };
+
+  return (
+    <section className="notfound">
+      <div className="notfound-wrapper">
+        <img src={NotFoundImage} alt="notFound" />
+        <Text
+          align="center"
+          fontColor="gil-blue"
+          fontSize="xxx-large"
+          fontWeight="bold"
+        >
+          페이지를 찾을 수 없습니다.
+        </Text>
+        <Text align="center" fontSize="xx-large">
+          입력하신 주소가 정확한지 다시 한번 확인해 주세요.
+        </Text>
+        <Button
+          innerText="메인으로 가기"
+          buttonColor="white"
+          textColor="gil-blue"
+          textSize="medium"
+          hasShadow
+          handleClick={handleButtonClick}
+        />
+      </div>
+    </section>
+  );
 };
 
 export default NotFound;
