42d26567cd4628390f1c881b4b18cb58b4d5d333
kyoung-jnn <rudwls468@gmail.com>
2022-01-09 23:59:09 +0900

    [GILJOB-147] feat: 플로팅 버튼 개발

---
+++ src/components/atoms/Floating/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import type { FC } from 'react';
+import { useLocation } from 'react-router-dom';
+import floating from '@src/assets/images/floating.png';
+import './style.scss';
+
+const Floating: FC = () => {
+  const { pathname } = useLocation();
+  const handleClick = () => {
+    window.open('https://open.kakao.com/o/gu470qTd', '_blank');
+  };
+
+  return !pathname.includes('/login') ? (
+    <button
+      id="kakao-talk-chaneel-chat-button"
+      className="_FLOATING_"
+      type="button"
+      onClick={handleClick}
+    >
+      <img src={floating} alt="floatingButton" />
+    </button>
+  ) : null;
+};
+
+export default React.memo(Floating);
